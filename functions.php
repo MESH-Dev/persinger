428dc2080c8dc608c24a46bd2b9a85fb8b348364
meshdesign <joshdodd@meshfresh.com>
2016-10-20 17:00:48 -0400

    change admin labels

--- functions.php
+++ functions.php
@@ -14,5 +14,30 @@ function my_custom_login_logo() {
 		    </style>';
 }
 
+function change_post_object_label() {
+    global $wp_post_types;
+    $labels = &$wp_post_types['post']->labels;
+    $labels->name = 'Projects';
+    $labels->singular_name = 'Project';
+    $labels->add_new = 'Add Project';
+    $labels->add_new_item = 'Add Project';
+    $labels->edit_item = 'Edit Projects';
+    $labels->new_item = 'Project';
+    $labels->view_item = 'View Project';
+    $labels->search_items = 'Search Projects';
+    $labels->not_found = 'No Projects found';
+    $labels->not_found_in_trash = 'No Projects found in Trash';
+}
+
+function change_post_menu_label() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = 'Projects';
+    $submenu['edit.php'][5][0] = 'Projects';
+    $submenu['edit.php'][10][0] = 'Add Projects';
+    echo '';
+}
+    add_action( 'init', 'change_post_object_label' );
+    add_action( 'admin_menu', 'change_post_menu_label' );
 
 ?>
