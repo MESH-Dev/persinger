573a4b58bb0bfeeec5040baa8fc0b8f40f9e85fd
meshdesign <joshdodd@meshfresh.com>
2016-10-20 15:23:25 -0400

    initial

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css" />
+	<title>Sass Test</title>
+</head>
+<body>
+
+<div class="row">
+	<div class="columns-12">
+		<div class="stuff">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="columns-6">
+		<div class="stuff">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+		</div>
+	</div>
+	<div class="columns-6">
+		<div class="stuff">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="columns-3">
+			<div class="stuff">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+			</div>
+ 
+		</div>
+		<div class="columns-9">
+			<div class="stuff">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="columns-4 no-padding ">
+		<div class="stuff">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+		</div>
+	</div>
+	<div class="columns-4 ">
+		<div class="stuff" style="background-color: red">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+		</div>
+	</div>
+	<div class="columns-4 ">
+		<div class="stuff">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit doloremque, repellat numquam ullam iure laboriosam ipsam eum aut velit dolore rerum, iusto eius! Dolorem quos iusto incidunt rem vero, maiores.
+		</div>
+
+</div>
+
+<div class="classname"></div><h1></h1> <div id="idmaeasdf"></div>
+
+
+	
+</body>
+</html>
+
+<!-- 
+
+
+
+
+
+
+<?php// get_header(); ?>
+
+<!-- <main id="content">
+
+	<div class="container">
+		<div class="row">
+			<div class="nine columns">
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+					<h1><?php the_title(); ?></h1>
+
+					<?php the_content(); ?>
+
+				<?php endwhile; ?>
+			</div>
+			<div class="three columns">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
+
+
+</main><!-- End of Content --> -->
+
+<?php //get_footer(); ?> -->
